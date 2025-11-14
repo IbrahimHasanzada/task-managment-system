a84658aba9d766ad95f5d0b0ea62e12dd66e8433
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-11-14 13:01:56 +0400

    feat(tasklist): add deleteTaskList method with authorization

--- src/modules/tasklist/tasklist.service.ts
+++ src/modules/tasklist/tasklist.service.ts
@@ -1,14 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { TaskListEntity } from "../../entities/tasklist.entity";
 import { CreateTaskListDto } from "./dto/create-tasklist.dto";
+import { ClsService } from "nestjs-cls";
 
 @Injectable()
 export class TaskListService {
 	constructor(
 		@InjectRepository(TaskListEntity)
-		private taskListRepo: Repository<TaskListEntity>
+		private taskListRepo: Repository<TaskListEntity>,
+		private cls: ClsService
 	) { }
 
 	async create(dto: CreateTaskListDto) {
@@ -18,6 +20,22 @@ export class TaskListService {
 
 	async listByFolder(folderId: number) {
 		return await this.taskListRepo.find({ where: { folderId }, order: { createdAt: 'DESC' } })
+	}
+
+	async deleteTaskList(id: number) {
+		let user = this.cls.get('user')
+
+		let taskList = await this.taskListRepo.findOne({ where: { id } })
+
+		if (!taskList) throw new NotFoundException('Siyahı tapılmadı')
+
+		if (user.role != 'admin') {
+			if (user.id != taskList.folder.ownerId) throw new UnauthorizedException('Siyahını silmək üçün icazəniz yoxdur')
+		}
+
+		await this.taskListRepo.delete({ id })
+
+		return { message: "Siyahı uğurla silindi" }
 	}
 }
 
